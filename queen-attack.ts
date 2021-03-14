7ffb3e405223a46a872bc3759aeb4dc88d869ad9
Lewis Nkwo <lewisnkwo@live.co.uk>
2021-03-14 13:51:44 +0000

    All tests passing

--- queen-attack.ts
+++ queen-attack.ts
@@ -31,6 +31,8 @@ export default class QueenAttack<T extends BoardSquare> {
 
     const positionOne = this.positionQueen(this.white, "W", this.defaultBoard);
     const positionTwo = this.positionQueen(this.black, "B", positionOne);
+
+    // FOR TEST: toString representation
     this.board = this.toBoardString(positionTwo).join("");
   }
 
@@ -51,7 +53,7 @@ export default class QueenAttack<T extends BoardSquare> {
       let stringRow: string[] = [];
 
       for (let i = 0; i < 8; i++) {
-        stringRow = [...stringRow, ...row[i].toString()];
+        stringRow = [...stringRow, row[i].toString()];
         if (i < 7) {
           stringRow = [...stringRow, " "];
         }
@@ -61,8 +63,31 @@ export default class QueenAttack<T extends BoardSquare> {
 
     let board: string[] = [];
     rows.forEach((val) => {
-      board = [...board, ...`${toStringRow(val).join("")}${"\n"}`];
+      board = [...board, `${toStringRow(val).join("")}${"\n"}`];
     });
     return board;
+  };
+
+  public canAttack = (): boolean => {
+    const whiteQueenRow = this.white[0] as number;
+    const whiteQueenColumn = this.white[1] as number;
+    const blackQueenRow = this.black[0] as number;
+    const blackQueenColumn = this.black[1] as number;
+
+    const canAttackDiagonally = (): boolean =>
+      (whiteQueenRow === whiteQueenColumn &&
+        blackQueenRow === blackQueenColumn) ||
+      whiteQueenRow - blackQueenRow === whiteQueenColumn - blackQueenColumn ||
+      whiteQueenRow - blackQueenRow === blackQueenColumn - whiteQueenColumn;
+
+    if (
+      whiteQueenRow === blackQueenRow ||
+      whiteQueenColumn === blackQueenColumn
+    ) {
+      return true;
+    } else if (canAttackDiagonally()) {
+      return true;
+    }
+    return false;
   };
 }
